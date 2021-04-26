832460961988a5b0d1abb521c17c128c5e12b066
miftah06 <izharuddinmiftah@gmail.com@users.noreply.github.com>
2021-04-26 20:44:28 +0800

    fix branch! This reverts commit bf0fb5d6

---
+++ Requests/_init_.py
@@ -0,0 +1,17 @@
+import null
+import got
+from index import grabbing
+
+if grabbing: null
+print("button tidak berjalan")
+
+# noinspection PyRedeclaration
+pwd = 'repr(pwd) = 0'
+
+# noinspection PyRedeclaration
+pwd = 'input.codes = txt'
+
+get = "'(repr(pwd)'= 'txt'"
+get = 'txt'
+if got: index = get
+print("maaf,anda kurang beruntung. mohon coba lagi")
---
+++ Requests/index.py
@@ -0,0 +1,28 @@
+from operator import index
+from pip._vendor.requests import get
+import get as requests
+import btn
+
+
+
+def grabbing(): pass
+
+requests.get = "script.id = {0}"
+requests.get = "0 == input.codes"
+requests.get = "script.id = {alpha}"
+requests.get = "alpha == input.codes"
+get("http://facebook.com")
+
+
+
+def pwd(password): pass
+
+if  btn.index =='':
+    print("sandi tidak dapat di peroleh")
+elif pwd: bool = btn.index == {"id=u_0_d_R5", "id=u_0_i_7B", "id=u_0_j_LT"}
+print("button tidak dapat mendeteksikan sandi")
+if index =='':
+    get('txt')
+    print("gagal memakai sandi")
+
+
